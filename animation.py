efbbd00b932a453cb907093ea7f96b1c076a68d9
yashnarang000 <yashnarang207@gmail.com>
2025-07-20 19:50:52 +0530

    add blink mechanism

--- animation.py
+++ animation.py
@@ -1,19 +1,11 @@
 import pygame
 from  facetracker_module.facetracker import Tracker
 import threading
-import queue
-
-q1 = queue.Queue()
-q2 = queue.Queue()
+import time
 
 pygame.init()
 
 screen = pygame.display.set_mode((0, 0))
-
-pupil1 = pygame.Vector2(y=screen.get_height() / 2)
-pupil2 = pygame.Vector2(y=screen.get_height() / 2)
-
-lid = pygame.Vector2(y=screen.get_height() / 2 - 40)
 
 dt = 1
 
@@ -51,7 +43,7 @@ def move_pupil(result, output_image, timestamp_ms):
             elif centre_x < min_x:
                 centre_x = min_x
 
-            print(x, y)
+            # print(x, y)
 
 
 
@@ -63,7 +55,85 @@ thread1 = threading.Thread(target = tracking.start, kwargs={'looping_condition':
 thread1.start()
 # thread2.start()
 
+blinks = 0
+completion = False
+
+def blink(prevBlinkTime, timePeriod, timePerBlink, top_lid, bottom_lid):
+    '''
+    speed: this parameter is in seconds/blink
+    '''
+    global blinks, prevTop, completion
+
+    BlinkTime = time.time()
+    
+    # if int(BlinkTime - prevBlinkTime) == blinks:
+    if (int(BlinkTime - prevBlinkTime)) % timePeriod == 0:
+        completion = False
+        prevTop = top_lid.y
+        # print(int(BlinkTime - prevBlinkTime))
+    
+    else:
+        # print(int(BlinkTime - prevBlinkTime))
+        if completion == False:
+            # print(int(BlinkTime - prevBlinkTime))
+
+            speed = 0.6 / timePerBlink
+
+            if top_lid.y == screen.get_height() / 2 + 40 and bottom_lid.y == -40: 
+
+                if prevTop < top_lid.y:
+                    # have just opened
+                    completion = True
+                    # print("Completed")
+                else:
+                    # starting to close
+                    # print("close")
+
+                    prevTop = top_lid.y
+
+                    top_lid.y -= speed    
+                    bottom_lid.y += speed
+
+            elif int(top_lid.y) == screen.get_height() / 2 and int(bottom_lid.y) == 0:
+                # starting to open
+                # print("open")
+
+                prevTop = top_lid.y
+
+                top_lid.y += speed
+                bottom_lid.y -= speed
+
+            else:
+                if prevTop > top_lid.y:
+                    # closing
+                    # print("closing")
+
+                    prevTop = top_lid.y
+
+                    top_lid.y -= speed
+                    bottom_lid.y += speed
+                
+                elif prevTop < top_lid.y:
+                    # opening
+                    # print("opening")
+
+                    prevTop = top_lid.y
+
+                    top_lid.y += speed
+                    bottom_lid.y -= speed
+                
+                else:
+                    print("BLINK ERROR!")
+
+pupil1 = pygame.Vector2(y=screen.get_height() / 2)
+pupil2 = pygame.Vector2(y=screen.get_height() / 2)
+
+toplid = pygame.Vector2(y=screen.get_height() / 2 + 40)
+bottomlid = pygame.Vector2(y=-40)
+
 running = True
+
+prevTime = time.time()
 
 while running:
 
@@ -77,11 +147,20 @@ while running:
 
     pygame.draw.circle(screen, "black", pupil1, 40)
     pygame.draw.circle(screen, "black", pupil2, 40)
+
+    # blinking logic starts here
+
+    topRect = pygame.Rect(toplid.x, toplid.y, screen.get_width(), screen.get_height() / 2)
+    bottomRect = pygame.Rect(bottomlid.x, bottomlid.y, screen.get_width(), screen.get_height() / 2)
+
+    pygame.draw.rect(screen, "white", topRect)
+    pygame.draw.rect(screen, "white", bottomRect)
+
+    blink(prevTime, 5, 1, toplid, bottomlid)
+
+    # blinking logic ends here
+
     pygame.draw.line(screen, "black", (pupil2.x, pupil2.y), (pupil1.x, pupil1.y), 10)
-
-    # blinking logic starts from here
-
-    # pygame.draw.rect(screen, "white", (lid.x, lid.y, ))
 
     if keys[pygame.K_ESCAPE]:
         running = False
